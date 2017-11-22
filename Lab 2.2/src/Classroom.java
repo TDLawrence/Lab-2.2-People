106108f85f35de36127de631754b12eddde1d8fb
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2017-11-22 09:24:42 -0500

    getGpa();

--- Lab 2.2/src/Classroom.java
+++ Lab 2.2/src/Classroom.java
@@ -1,16 +1,26 @@
 package src;
 public class Classroom {
-private Person[]students;
-private Person teacher;
+private Student[]students;
+private Teacher teacher;
 
-public Classroom(Person[] students, Person teacher, String subject, double gpa)
+public Classroom(Student[] students, Teacher teacher, String subject)
 {
 	this.students = students;
 	this.teacher = teacher;
-	Student.gpa = gpa;
+	
 }
 
 public String getSubject() {
-	return Person.Teacher.getSubject();
+	return this.teacher.getSubject();
 }
+
+public double classAverage()
+{
+	double sum = 0;
+	for (int n = 0; n <= students.length; n++) {
+		sum = sum + students[n].getGpa();
+	}
+	return sum / students.length;
+}
+
 }
